56a995ad221a923326206c5df710302c5688f021
tkealamakia <tkealamakia@gmail.com>
2025-03-24 07:54:11 -0600

    Stuff

--- src/main/java/com/kealamakia/main/GamePanel.java
+++ src/main/java/com/kealamakia/main/GamePanel.java
@@ -1,5 +1,8 @@
 package com.kealamakia.main;
 
+import com.kealamakia.entity.Player;
+import com.kealamakia.tile.TileManager;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,7 +10,7 @@ public class GamePanel extends JPanel implements Runnable {
   final int orignalTileSize = 16; // 16x16 tile
   final int scale = 3;
 
-  final int tileSize = orignalTileSize * scale; // 48x48 title
+  public final int tileSize = orignalTileSize * scale; // 48x48 title
   final int maxScreenCol = 16;
   final int maxScreenRow = 12;
   final int screenWidth = tileSize * maxScreenCol; // 768 pixels
@@ -15,12 +18,10 @@ public class GamePanel extends JPanel implements Runnable {
 
   int FPS = 60;
 
+  TileManager tileManager = new TileManager(this);
   KeyHandler keyH = new KeyHandler();
   Thread gameThread;
-
-  int playerX = 100;
-  int playerY = 100;
-  int playerSpeed = 4;
+  Player player = new Player(this, keyH);
 
   public GamePanel() {
     this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -92,22 +93,14 @@ public class GamePanel extends JPanel implements Runnable {
 //  }
 
   public void update() {
-    if (keyH.upPressed == true) {
-      playerY -= playerSpeed;
-    } else if (keyH.downPressed == true) {
-      playerY += playerSpeed;
-    } else if (keyH.leftPressed == true) {
-      playerX -= playerSpeed;
-    } else if (keyH.rightPressed == true) {
-      playerX += playerSpeed;
-    }
+    player.update();
   }
 
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D) g;
-    g2.setColor(Color.WHITE);
-    g2.fillRect(playerX, playerY, tileSize, tileSize);
+    tileManager.draw(g2);
+    player.draw(g2);
     g2.dispose();
   }
 }
